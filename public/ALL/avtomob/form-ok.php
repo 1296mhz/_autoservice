c92ba3c2546f7368aff5ed04ccf7e593eeb45e16
cshlovjah <cshlovjah@gmail.com>
2016-02-15 17:13:07 +0500

    fuck

---
+++ public/ALL/avtomob/form-ok.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Заявка принята</title>
+<meta name="generator" content="WYSIWYG Web Builder 9 - http://www.wysiwygwebbuilder.com">
+<style type="text/css">
+body
+{
+   font-size: 8px;
+   line-height: 1.1875;
+   margin: 0;
+   padding: 0;
+   background-color: #F0FFF0;
+   color: #000000;
+}
+</style>
+<style type="text/css">
+a
+{
+   color: #0000FF;
+   text-decoration: underline;
+}
+a:visited
+{
+   color: #800080;
+}
+a:active
+{
+   color: #FF0000;
+}
+a:hover
+{
+   color: #0000FF;
+   text-decoration: underline;
+}
+</style>
+<style type="text/css">
+#wb_Text3 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: center;
+}
+#wb_Text3 div
+{
+   text-align: center;
+}
+#Image1
+{
+   border: 0px #000000 solid;
+}
+#wb_Text1 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: center;
+}
+#wb_Text1 div
+{
+   text-align: center;
+}
+</style>
+</head>
+<body>
+<div id="wb_Shape10" style="position:absolute;left:50px;top:24px;width:502px;height:362px;z-index:0;">
+<img src="images/img0017.png" id="Shape10" alt="" style="border-width:0;width:502px;height:362px;"></div>
+
+<div id="wb_Text3" style="position:absolute;left:107px;top:151px;width:390px;height:66px;text-align:center;z-index:2;">
+<span style="color:#228B22;font-family:Tahoma;font-size:27px;">Ваша заявка принята.<br>Спасибо!</span></div>
+<div id="wb_Image1" style="position:absolute;left:269px;top:78px;width:64px;height:64px;z-index:3;">
+<img src="images/64Px%20-%20146.png" id="Image1" alt="" style="width:64px;height:64px;"></div>
+<div id="wb_Text1" style="position:absolute;left:146px;top:223px;width:291px;height:48px;text-align:center;z-index:4;">
+<span style="color:#696969;font-family:Tahoma;font-size:20px;"> Наш менеджер свяжется с Вами в течении часа.</span></div>
+</body>
+</html>
---
+++ public/cal/test/config.php
@@ -0,0 +1,3 @@
+<?php
+$con = mysqli_connect('localhost','root','P@$$word','bootstrap_calendar');
+?>
